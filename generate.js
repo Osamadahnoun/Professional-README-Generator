ff3445304ce31ed77823684deb415f0e469c937a
Osama Dahnoun <omdahnou@ncsu.edu>
2021-12-10 19:03:42 -0500

    app takes user input and prints onto generated README

---
+++ generate.js
@@ -0,0 +1,39 @@
+module.exports = data => {
+    const {title, description, installation, usage, screenshot, contributing, Test, username, email, license} = data;
+    return `
+# ${title}
+
+## Description
+* ${description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#questions)
+
+## Installation
+* ${installation}
+
+## Usage
+* ${usage}
+* ${screenshot}
+
+## Contributing
+* ${contributing}
+
+## Test
+* ${Test}
+
+## Questions
+* [My Github Profile!](github.com/${username})
+* Contact me at ${email}
+
+##Badges
+* 
+
+## License
+* Covered under the ${license}
+ `
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generate = require('./generate')
 
 // TODO: Create an array of questions for user input
-const questions = ['What is the title of your project?', 'Please Provide a Description for Your Project', 'What are the steps required to install your project? Recommended (Provide a numbered list).', 'Provide instructions and examples for use. (A screenshot can be added in the following step).', 'Would you like to add a screenshot to the usage section?', 'Please add screenshot using this syntax: ![alt text](assets/images/screenshot.png)', 
-'What are your contribution guidelines for this project?', 'Would you like to write tests for your application?', 'Write tests for your application. Then provide examples on how to run them.', 'What is your github username?', 'What email would you like to be contacted with?'];
+const questions = ['What is the title of your project?', 'Please Provide a Description for Your Project.', 'What are the step-by-step instructions required to install your project?.', 'Provide instructions and examples for use.', 'Please add screenshot using this syntax: ![alt text](assets/images/screenshot.png)', 
+'What are your contribution guidelines for this project?', 'Would you like to write tests for your application?', 'Write tests for your application. Then provide examples on how to run them.', 'What is your github username?', 'What email would you like to be contacted with?', 'Which README license would you like to use?'];
 
 
 const askQuestions = data => {
@@ -29,59 +30,56 @@ const askQuestions = data => {
             name: 'usage',
             message: questions[3]
         },
-        {
-            type: 'confirm',
-            name: 'confirmScreenShot',
-            message: questions[4]
-        },
+        // {
+        //     type: 'confirm',
+        //     name: 'confirmScreenShot',
+        //     message: questions[4]
+        // },
         {
             type: 'input',
             name: 'screenshot',
-            message: questions[5],
-            when: ({ confirmScreenShot }) => confirmScreenShot
+            message: questions[4],
         },
         {
             type: 'input',
             name: 'contributing',
-            message: questions[6]
+            message: questions[5]
         },
         {
             type: 'confirm',
             name: 'confirmTest',
-            message: questions[7]
+            message: questions[6]
         },
         {
             type: 'input',
             name: 'Test',
-            message: questions[8],
+            message: questions[7],
             when: ({ confirmTest }) => confirmTest
         },
-        // {
-        //     type: 'checkbox',
-        //     name: 'languages',
-        //     message: 'What did you this project with? (Check all that apply)',
-        //     choices: ['']
-        //   },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: questions[10],
+            choices: ['MIT License', 'Apache License 2.0', 'Boost Software License 1.0']
+          },
         {
             type: 'input',
             name: 'username',
-            message: questions[9]
+            message: questions[8]
         },
         {
             type: 'input',
             name: 'email',
-            message: questions[10]
+            message: questions[9]
         },
     ]);
 };
 
 askQuestions().then(answers => {
-    console.log(answers)
-})
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('README.md', 'HI', err => {
+    // console.log(answers.title)
+    // console.log(answers.description)
+    const pageMarkup = generate(answers);
+    fs.writeFile('README.md', pageMarkup, err => {
           if (err) {
           console.log(err);
            return;
@@ -89,12 +87,16 @@ function writeToFile(fileName, data) {
         console.log('Page created! Check out index.html in this directory to see it!');
     }
     )
-}
+})
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+   
+// }
 
 // TODO: Create a function to initialize app
-function init() {
-    askQuestions()
-}
+// function init() {
+// }
 
 // Function call to initialize app
-init();
+// init();
